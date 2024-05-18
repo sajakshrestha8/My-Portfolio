6eea5fe73de1aabb90e3e463f0fbf3a2e6700fb7
sajakshrestha8 <shresthasajak123@gmail.com>
2024-05-18 23:25:09 +0545

    Updated minor hovers and works

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Info from "./assets/Components/Info";
 import Myprojects from "./assets/Components/Myprojects";
 import Work from "./assets/Components/Work";
 import Project1 from "./assets/project-1.png";
+import Project2 from "./assets/project-2.png";
+import Project3 from "./assets/project-3.png";
+import Project4 from "./assets/project-4.png";
+import Project5 from "./assets/project-5.png";
 
 function App() {
   return (
@@ -15,12 +19,55 @@ function App() {
       <section className="work-section">
         <Work />
       </section>
-      <section className="works">
+      <section className="works-wrapper">
         <Myprojects
           img={Project1}
           name={"Twitter Clone"}
           link={
             <a href="https://sajak-twitterclone.netlify.app/" className="link">
+              --{">"} View my project
+            </a>
+          }
+        />
+
+        <Myprojects
+          img={Project2}
+          name={"Redcross Website"}
+          link={
+            <a
+              href="http://localhost/bloodbankfront/index.php"
+              className="link"
+            >
+              --{">"} View my project
+            </a>
+          }
+        />
+
+        <Myprojects
+          img={Project3}
+          name={"To-Do-List"}
+          link={
+            <a href="https://sajak-to-do-list.netlify.app/" className="link">
+              --{">"} View my project
+            </a>
+          }
+        />
+
+        <Myprojects
+          img={Project4}
+          name={"Timer"}
+          link={
+            <a href="https://sajak-to-do-list.netlify.app/" className="link">
+              --{">"} View my project
+            </a>
+          }
+        />
+
+        <Myprojects
+          img={Project5}
+          name={"Calculator"}
+          link={
+            <a href="https://sajak-calculator.netlify.app" className="link">
               --{">"} View my project
             </a>
           }
--- src/assets/Components/Info.jsx
+++ src/assets/Components/Info.jsx
@@ -1,6 +1,7 @@
 import Facebook from "../Facebook.png";
 import Instagram from "../instagram.png";
 import Linkedin from "../linkedin.png";
+import Profile from "../profile.png";
 
 function Info() {
   return (
@@ -8,30 +9,42 @@ function Info() {
       <div className="landing-page-wrapper">
         <span className="name">Sajak Shrestha</span>
 
-        <div className="heading">
-          <span className="frontend-head">Frontend</span>
-          <span className="frontend-head">Developer.</span>
-        </div>
-        <div>
-          <span className="frontend-desc">
-            I like to design and create solid and scalable
-          </span>
-          <br />
-          <span className="frontend-desc">product with great UI.</span>
-        </div>
-        <span className="know-head">Want to know me personally?</span>
-      </div>
+        <div className="image-frontend">
+          <div>
+            <div className="heading">
+              <span className="frontend-head">Frontend</span>
+              <span className="frontend-head">Developer.</span>
+            </div>
 
-      {/* Social media */}
-      <div className="social-media-wrapper">
-        <div>
-          <img className="facebook" src={Facebook} alt="" />
+            <div>
+              <span className="frontend-desc">
+                I like to design and create solid and scalable
+              </span>
+              <br />
+              <span className="frontend-desc">product with great UI.</span>
+            </div>
+          </div>
+
+          <div className="profile">
+            <img src={Profile}></img>
+          </div>
         </div>
+
         <div>
-          <img className="instagram" src={Instagram} alt="" />
-        </div>
-        <div>
-          <img className="linkedin" src={Linkedin} alt="" />
+          <span className="know-head">Want to know me personally?</span>
+
+          {/* Social media */}
+          <div className="social-media-wrapper">
+            <div>
+              <img className="facebook" src={Facebook} alt="" />
+            </div>
+            <div>
+              <img className="instagram" src={Instagram} alt="" />
+            </div>
+            <div>
+              <img className="linkedin" src={Linkedin} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </>
